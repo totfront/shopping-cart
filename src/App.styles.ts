b2d9f14fa209490667bda6fe7e6a530df684715e
totfront <totfront@gmail.com>
2022-01-26 13:25:09 +0100

    feat: basic application made of open-api items

---
+++ src/App.styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+import IconButton from "@material-ui/core/IconButton"
+
+export const Wrapper = styled.div`
+    margin: 40px;
+`
+
+export const StyledButton = styled(IconButton)`
+    position: fixed;
+    z-index: 1;
+    top: 20px;
+    right: 20px;
+`
